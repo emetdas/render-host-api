3b3a5afa6f946451b95b7e3d9451a15d5ca6f842
Emet Das <you@example.com>
2023-11-04 11:54:07 +0600

    ecommerce api testing

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql');
+require('dotenv').config();
+
+const api = express();
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+};
+
+const connection = mysql.createConnection(dbConfig);
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database: ' + err);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+api.get('/', (req, res) => {
+  const query = 'SELECT * FROM products';
+
+  connection.query(query, (error, results) => {
+    if (error) {
+      console.error('Error executing the query: ' + error);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    res.json({ products: results });
+  });
+});
+
+api.listen(process.env.PORT, () => {
+  console.log('Server is running on port ' + process.env.PORT);
+});
